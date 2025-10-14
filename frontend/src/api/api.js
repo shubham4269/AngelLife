af4ea4267e0d51e3bdad7e03efee59c5042adb10
shubham kumar <shubhamkumarchoudharyy@gmail.com>
2025-10-14 06:08:37 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://angellife-backend.onrender.com',
+  baseURL: 'https://angellife-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default api;
+
 
